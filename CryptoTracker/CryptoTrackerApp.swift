0504cc2a93df318e16716b301dab1dfed1823b22
Doğukaan Kılıçarslan <dogukaan.kilicarslan@hepsiburada.com>
2022-11-09 21:25:38 +0300

    Initial Commit

---
+++ CryptoTracker/CryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerApp.swift
+//  CryptoTracker
+//
+//  Created by Doğukaan Kılıçarslan on 9.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
